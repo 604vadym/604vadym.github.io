1aaf996dc359dc5fad9c288b6a2262c855d79bd2
604vadym <604vadym@proton.me>
2026-07-04 10:19:42 +0300

    feat: implement Task - factorial via recursion

--- javascript-start/04.homework-functions/script/main.js
+++ javascript-start/04.homework-functions/script/main.js
@@ -7,3 +7,23 @@ console.log("JavaScriptStart #4. Домашнє завдання. Основи J
  *
  * Створіть функцію factorial, яка приймає одне число і повертає його факторіал. Факторіал числа n обчислюється як добуток всіх цілих чисел від 1 до n.
  */
+
+function factorial(number) {
+  if (number < 0) {
+    return NaN;
+  }
+  if (number === 0 || number === 1) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
+
+console.log("-5! =", factorial(-5));
+console.log("0! =", factorial(0));
+console.log("1! =", factorial(1));
+console.log("2! =", factorial(2));
+console.log("3! =", factorial(3));
+console.log("4! =", factorial(4));
+console.log("5! =", factorial(5));
+console.log("8! =", factorial(8));
+console.log("15! =", factorial(15));
