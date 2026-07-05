fd2605cca03a94c001c7c84745e12ce6d32222cf
604vadym <604vadym@proton.me>
2026-07-05 13:40:19 +0300

    feat: implement Task

--- javascript-start-javascript-core/05.homework-switch-case/script/main.js
+++ javascript-start-javascript-core/05.homework-switch-case/script/main.js
@@ -11,3 +11,30 @@ console.log(
  *
  * Наприклад: якщо число 5 - вивести “П’ятниця”, якшо 2 - “Вівторок”
  */
+
+const number = Math.floor(Math.random() * 7) + 1;
+console.log("Generated number =", number);
+
+switch (number) {
+  case 1:
+    console.log("Понеділок");
+    break;
+  case 2:
+    console.log("Вівторок");
+    break;
+  case 3:
+    console.log("Середа");
+    break;
+  case 4:
+    console.log("Четвер");
+    break;
+  case 5:
+    console.log("П’ятниця");
+    break;
+  case 6:
+    console.log("Субота");
+    break;
+  case 7:
+    console.log("Неділя");
+    break;
+}
