56dee90ede0428684001576a4ead3074399a2021
604vadym <604vadym@proton.me>
2026-07-06 20:04:53 +0300

    feat: implement Task - data types

--- javascript-bogdan-stashchuk-course/06-data-types/finish.js
+++ javascript-bogdan-stashchuk-course/06-data-types/finish.js
@@ -12,3 +12,19 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const string = "hello";
+const number = 7;
+const bool = true;
+const nullVar = null;
+const undefinedVar = undefined;
+const object = {};
+const arr = [];
+
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof bool);
+console.log(typeof nullVar);
+console.log(typeof undefinedVar);
+console.log(typeof object);
+console.log(typeof arr);
