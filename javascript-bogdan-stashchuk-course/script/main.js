27afb1dd95f1cbf0073792ae309d0af0a50fb686
604vadym <604vadym@proton.me>
2026-07-09 10:01:50 +0300

    refactor: replace if else with map object

--- javascript-bogdan-stashchuk-course/script/main.js
+++ javascript-bogdan-stashchuk-course/script/main.js
@@ -1,6 +1,89 @@
 const terminal = document.getElementById("terminal");
 const select = document.getElementById("hw-select");
 
+const homeworkMap = {
+    hw1: "01-variable-declaration/finish.js",
+    hw2: "02-value-reassignment/finish.js",
+    hw3: "03-numbers-multiplication/finish.js",
+    hw4: "04-strings-concatenation/finish.js",
+    hw5: "05-comments/finish.js",
+    hw6: "06-data-types/finish.js",
+    hw7: "07-regular-function/finish.js",
+    hw8: "08-function-expression/finish.js",
+    hw9: "09-arrow-function/finish.js",
+    hw10: "10-jsdoc/finish.js",
+    hw11: "11-callback-function/finish.js",
+    hw12: "12-create-object/finish.js",
+    hw13: "13-string-length/finish.js",
+    hw14: "14-uppercase-string/finish.js",
+    hw15: "15-replace-part-string/finish.js",
+    hw16: "16-create-array/finish.js",
+    hw17: "17-replace-element-array/finish.js",
+    hw18: "18-iterate-over-array/finish.js",
+    hw19: "19-push-to-array/finish.js",
+    hw20: "20-date/finish.js",
+    hw21: "21-compare-variables/finish.js",
+    hw22: "22-division-remainder/finish.js",
+    hw23: "23-or-and-operators/finish.js",
+    hw24: "24-alternative-binary-operators/finish.js",
+    hw25: "25-expression-or-statement/finish.js",
+    hw26: "26-what-will-be-logged/finish.js",
+    hw27: "27-const/finish.js",
+    hw28: "28-let/finish.js",
+    hw29: "29-strict-mode/finish.js",
+    hw30: "30-callback-function/finish.js",
+    hw31: "31-intervals-timeouts/finish.js",
+    hw32: "32-add-start-of-the-array/finish.js",
+    hw33: "33-add-at-index-array/finish.js",
+    hw34: "34-array-of-objects/finish.js",
+    hw35: "35-object-iteration/finish.js",
+    hw36: "36-random-numbers/finish.js",
+    hw37: "37-for-in-loop/finish.js",
+    hw38: "38-ternary-operator/finish.js",
+    hw39: "39-foreach-with-index/finish.js",
+    hw40: "40-map-json-to-object/finish.js",
+    hw41: "41-find-single-post/finish.js",
+    hw42: "42-is-array-sorted-or-not/finish.js",
+    hw43: "43-compare-two-arrays/finish.js",
+    hw44: "44-element-is-in-array/finish.js",
+    hw45: "45-includes-object-or-array/finish.js",
+    hw46: "46-push-to-array-if-not-exists/finish.js",
+    hw47: "47-reduce-array-of-objects/finish.js",
+    hw48: "48-reduce-to-object/finish.js",
+    hw49: "49-sort-array-of-objects/finish.js",
+    hw50: "50-template-literals/finish.js",
+    hw51: "51-rest-and-spread-operators/finish.js",
+    hw52: "52-default-function-parameters/finish.js",
+    hw53: "53-enhanced-object-literals/finish.js",
+    hw54: "54-array-destructuring/finish.js",
+    hw55: "55-destructure-function-result/finish.js",
+    hw56: "56-object-destructuring/finish.js",
+    hw57: "57-array-transformation/finish.js",
+    hw58: "58-delete-object-properties/finish.js",
+    hw59: "59-extend-array/finish.js",
+    hw60: "60-custom-push-method-for-arrays/finish.js",
+    hw61: "61-sum-numbers/finish.js",
+    hw62: "62-different-functions/finish.js",
+    hw63: "63-function-scope/finish.js",
+    hw64: "64-hoisting/finish.js",
+    hw65: "65-ternary-operator/finish.js",
+    hw66: "66-arrow-functions/finish.js",
+    hw67: "67-default-parameters/finish.js",
+    hw68: "68-presence-of-the-function-parameters/finish.js",
+    hw69: "69-object-destructuring/finish.js",
+    hw70: "70-destructuring-and-rest-operator/finish.js",
+    hw71: "71-spread-operator/finish.js",
+    hw72: "72-copy-array/finish.js",
+    hw73: "73-template-literals/finish.js",
+    hw74: "74-object-destructuring/finish.js",
+    hw75: "75-iterate-over-string/finish.js",
+    hw76: "76-swap-values/finish.js",
+    hw77: "77-function-closures/finish.js",
+    hw78: "78-classes/finish.js",
+    hw79: "79-iterate-over-object/finish.js",
+    hw80: "80-sum-of-positive-and-negative-numbers/finish.js",
+};
+
 function logToTerminal(...args) {
     const msg = args
         .map((arg) => {
@@ -36,166 +119,14 @@ select.addEventListener("change", (e) => {
     terminal.innerHTML =
         '<div class="line system">[System]: Executing script...</div><div class="line system">--------------------------------------------------</div>';
 
-    if (e.target.value === "hw1") {
-        runScriptInSandbox("01-variable-declaration/finish.js");
-    } else if (e.target.value === "hw2") {
-        runScriptInSandbox("02-value-reassignment/finish.js");
-    } else if (e.target.value === "hw3") {
-        runScriptInSandbox("03-numbers-multiplication/finish.js");
-    } else if (e.target.value === "hw4") {
-        runScriptInSandbox("04-strings-concatenation/finish.js");
-    } else if (e.target.value === "hw5") {
-        runScriptInSandbox("05-comments/finish.js");
-    } else if (e.target.value === "hw6") {
-        runScriptInSandbox("06-data-types/finish.js");
-    } else if (e.target.value === "hw7") {
-        runScriptInSandbox("07-regular-function/finish.js");
-    } else if (e.target.value === "hw8") {
-        runScriptInSandbox("08-function-expression/finish.js");
-    } else if (e.target.value === "hw9") {
-        runScriptInSandbox("09-arrow-function/finish.js");
-    } else if (e.target.value === "hw10") {
-        runScriptInSandbox("10-jsdoc/finish.js");
-    } else if (e.target.value === "hw11") {
-        runScriptInSandbox("11-callback-function/finish.js");
-    } else if (e.target.value === "hw12") {
-        runScriptInSandbox("12-create-object/finish.js");
-    } else if (e.target.value === "hw13") {
-        runScriptInSandbox("13-string-length/finish.js");
-    } else if (e.target.value === "hw14") {
-        runScriptInSandbox("14-uppercase-string/finish.js");
-    } else if (e.target.value === "hw15") {
-        runScriptInSandbox("15-replace-part-string/finish.js");
-    } else if (e.target.value === "hw16") {
-        runScriptInSandbox("16-create-array/finish.js");
-    } else if (e.target.value === "hw17") {
-        runScriptInSandbox("17-replace-element-array/finish.js");
-    } else if (e.target.value === "hw18") {
-        runScriptInSandbox("18-iterate-over-array/finish.js");
-    } else if (e.target.value === "hw19") {
-        runScriptInSandbox("19-push-to-array/finish.js");
-    } else if (e.target.value === "hw20") {
-        runScriptInSandbox("20-date/finish.js");
-    } else if (e.target.value === "hw21") {
-        runScriptInSandbox("21-compare-variables/finish.js");
-    } else if (e.target.value === "hw22") {
-        runScriptInSandbox("22-division-remainder/finish.js");
-    } else if (e.target.value === "hw23") {
-        runScriptInSandbox("23-or-and-operators/finish.js");
-    } else if (e.target.value === "hw24") {
-        runScriptInSandbox("24-alternative-binary-operators/finish.js");
-    } else if (e.target.value === "hw25") {
-        runScriptInSandbox("25-expression-or-statement/finish.js");
-    } else if (e.target.value === "hw26") {
-        runScriptInSandbox("26-what-will-be-logged/finish.js");
-    } else if (e.target.value === "hw27") {
-        runScriptInSandbox("27-const/finish.js");
-    } else if (e.target.value === "hw28") {
-        runScriptInSandbox("28-let/finish.js");
-    } else if (e.target.value === "hw29") {
-        runScriptInSandbox("29-strict-mode/finish.js");
-    } else if (e.target.value === "hw30") {
-        runScriptInSandbox("30-callback-function/finish.js");
-    } else if (e.target.value === "hw31") {
-        runScriptInSandbox("31-intervals-timeouts/finish.js");
-    } else if (e.target.value === "hw32") {
-        runScriptInSandbox("32-add-start-of-the-array/finish.js");
-    } else if (e.target.value === "hw33") {
-        runScriptInSandbox("33-add-at-index-array/finish.js");
-    } else if (e.target.value === "hw34") {
-        runScriptInSandbox("34-array-of-objects/finish.js");
-    } else if (e.target.value === "hw35") {
-        runScriptInSandbox("35-object-iteration/finish.js");
-    } else if (e.target.value === "hw36") {
-        runScriptInSandbox("36-random-numbers/finish.js");
-    } else if (e.target.value === "hw37") {
-        runScriptInSandbox("37-for-in-loop/finish.js");
-    } else if (e.target.value === "hw38") {
-        runScriptInSandbox("38-ternary-operator/finish.js");
-    } else if (e.target.value === "hw39") {
-        runScriptInSandbox("39-foreach-with-index/finish.js");
-    } else if (e.target.value === "hw40") {
-        runScriptInSandbox("40-map-json-to-object/finish.js");
-    } else if (e.target.value === "hw41") {
-        runScriptInSandbox("41-find-single-post/finish.js");
-    } else if (e.target.value === "hw42") {
-        runScriptInSandbox("42-is-array-sorted-or-not/finish.js");
-    } else if (e.target.value === "hw43") {
-        runScriptInSandbox("43-compare-two-arrays/finish.js");
-    } else if (e.target.value === "hw44") {
-        runScriptInSandbox("44-element-is-in-array/finish.js");
-    } else if (e.target.value === "hw45") {
-        runScriptInSandbox("45-includes-object-or-array/finish.js");
-    } else if (e.target.value === "hw46") {
-        runScriptInSandbox("46-push-to-array-if-not-exists/finish.js");
-    } else if (e.target.value === "hw47") {
-        runScriptInSandbox("47-reduce-array-of-objects/finish.js");
-    } else if (e.target.value === "hw48") {
-        runScriptInSandbox("48-reduce-to-object/finish.js");
-    } else if (e.target.value === "hw49") {
-        runScriptInSandbox("49-sort-array-of-objects/finish.js");
-    } else if (e.target.value === "hw50") {
-        runScriptInSandbox("50-template-literals/finish.js");
-    } else if (e.target.value === "hw51") {
-        runScriptInSandbox("51-rest-and-spread-operators/finish.js");
-    } else if (e.target.value === "hw52") {
-        runScriptInSandbox("52-default-function-parameters/finish.js");
-    } else if (e.target.value === "hw53") {
-        runScriptInSandbox("53-enhanced-object-literals/finish.js");
-    } else if (e.target.value === "hw54") {
-        runScriptInSandbox("54-array-destructuring/finish.js");
-    } else if (e.target.value === "hw55") {
-        runScriptInSandbox("55-destructure-function-result/finish.js");
-    } else if (e.target.value === "hw56") {
-        runScriptInSandbox("56-object-destructuring/finish.js");
-    } else if (e.target.value === "hw57") {
-        runScriptInSandbox("57-array-transformation/finish.js");
-    } else if (e.target.value === "hw58") {
-        runScriptInSandbox("58-delete-object-properties/finish.js");
-    } else if (e.target.value === "hw59") {
-        runScriptInSandbox("59-extend-array/finish.js");
-    } else if (e.target.value === "hw60") {
-        runScriptInSandbox("60-custom-push-method-for-arrays/finish.js");
-    } else if (e.target.value === "hw61") {
-        runScriptInSandbox("61-sum-numbers/finish.js");
-    } else if (e.target.value === "hw62") {
-        runScriptInSandbox("62-different-functions/finish.js");
-    } else if (e.target.value === "hw63") {
-        runScriptInSandbox("63-function-scope/finish.js");
-    } else if (e.target.value === "hw64") {
-        runScriptInSandbox("64-hoisting/finish.js");
-    } else if (e.target.value === "hw65") {
-        runScriptInSandbox("65-ternary-operator/finish.js");
-    } else if (e.target.value === "hw66") {
-        runScriptInSandbox("66-arrow-functions/finish.js");
-    } else if (e.target.value === "hw67") {
-        runScriptInSandbox("67-default-parameters/finish.js");
-    } else if (e.target.value === "hw68") {
-        runScriptInSandbox("68-presence-of-the-function-parameters/finish.js");
-    } else if (e.target.value === "hw69") {
-        runScriptInSandbox("69-object-destructuring/finish.js");
-    } else if (e.target.value === "hw70") {
-        runScriptInSandbox("70-destructuring-and-rest-operator/finish.js");
-    } else if (e.target.value === "hw71") {
-        runScriptInSandbox("71-spread-operator/finish.js");
-    } else if (e.target.value === "hw72") {
-        runScriptInSandbox("72-copy-array/finish.js");
-    } else if (e.target.value === "hw73") {
-        runScriptInSandbox("73-template-literals/finish.js");
-    } else if (e.target.value === "hw74") {
-        runScriptInSandbox("74-object-destructuring/finish.js");
-    } else if (e.target.value === "hw75") {
-        runScriptInSandbox("75-iterate-over-string/finish.js");
-    } else if (e.target.value === "hw76") {
-        runScriptInSandbox("76-swap-values/finish.js");
-    } else if (e.target.value === "hw77") {
-        runScriptInSandbox("77-function-closures/finish.js");
-    } else if (e.target.value === "hw78") {
-        runScriptInSandbox("78-classes/finish.js");
-    } else if (e.target.value === "hw79") {
-        runScriptInSandbox("79-iterate-over-object/finish.js");
-    } else if (e.target.value === "hw80") {
-        runScriptInSandbox("80-sum-of-positive-and-negative-numbers/finish.js");
+    const path = homeworkMap[e.target.value];
+
+    if (path) {
+        runScriptInSandbox(path);
+    } else {
+        logErrorToTerminal(
+            `Path for homework key "${e.target.value}" was not found.`,
+        );
     }
 });
 
