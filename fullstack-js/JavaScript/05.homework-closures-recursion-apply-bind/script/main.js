581d6d28bd52c4133616cd75964fe42cec1e20cc
604vadym <604vadym@proton.me>
2026-07-09 12:50:52 +0300

    feat: implement Task 2

--- fullstack-js/JavaScript/05.homework-closures-recursion-apply-bind/script/main.js
+++ fullstack-js/JavaScript/05.homework-closures-recursion-apply-bind/script/main.js
@@ -21,6 +21,7 @@ console.log(
 
 const counter = (function () {
     let currentValue = 0;
+
     return (n) => {
         if (typeof n === "number" && !Number.isNaN(n)) {
             currentValue = n;
@@ -58,22 +59,41 @@ console.log(
         "******************************",
 );
 
-// const counterFactory = function () {}
+const counterFactory = (function () {
+    let currentValue = 0;
 
-// console.log(counterFactory.value()) // 0
-// counterFactory.increment()
-// counterFactory.increment()
-// counterFactory.increment()
-// console.log(counterFactory.value()) // 3
-// counterFactory.decrement()
-// counterFactory.decrement()
-// console.log(counterFactory.value()) // 1
-// console.log(counterFactory.value(100)) // 100
-// counterFactory.decrement()
-// console.log(counterFactory.value()) // 99
-// console.log(counterFactory.value(200)) // 200
-// counterFactory.increment()
-// console.log(counterFactory.value()) // 201
+    return {
+        value(n) {
+            if (typeof n === "number" && !Number.isNaN(n)) {
+                currentValue = n;
+            }
+            return currentValue;
+        },
+
+        increment() {
+            currentValue++;
+        },
+
+        decrement() {
+            currentValue--;
+        },
+    };
+})();
+
+console.log(counterFactory.value()); // 0
+counterFactory.increment();
+counterFactory.increment();
+counterFactory.increment();
+console.log(counterFactory.value()); // 3
+counterFactory.decrement();
+counterFactory.decrement();
+console.log(counterFactory.value()); // 1
+console.log(counterFactory.value(100)); // 100
+counterFactory.decrement();
+console.log(counterFactory.value()); // 99
+console.log(counterFactory.value(200)); // 200
+counterFactory.increment();
+console.log(counterFactory.value()); // 201
 
 /*
  * #3
