02245f54703c3e9f8ba0df221b921f434e84dce0
604vadym <604vadym@proton.me>
2026-07-14 10:20:36 +0300

    feat: implement Task 2 - arrays in 5 variants

--- javascript-start-javascript-core/07.homework-arrays/script/main.js
+++ javascript-start-javascript-core/07.homework-arrays/script/main.js
@@ -1,11 +1,11 @@
 "use strict";
 
 console.log(
-    "JavaScript Core #7. Домашнє завдання. Основи JavaScript: Масиви (Arrays)",
+    "JavaScriptStart & JavaScript Core #7. Домашнє завдання. Основи JavaScript: Масиви (Arrays)",
 );
 
 /**
- * Створити масив чисел розміром 10 елементів.
+ * #1 Створити масив чисел розміром 10 елементів.
  * Записати випадкове(random) число в кожну ячейку масиву
  * Вивести масив на екран
  * складне:
@@ -20,16 +20,85 @@ console.log(
  * Важливо: алгоритм має працювати для масиву любого розміру
  */
 
-const arr = new Array(10);
+console.log(
+    "******************************\n" +
+        "Task 1\n" +
+        "******************************",
+);
 
-for (let i = 0; i < arr.length; i++) {
-    arr[i] = Math.floor(Math.random() * 21);
+const numbers = new Array(10);
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = Math.floor(Math.random() * 21);
 }
 
-console.log(arr);
+console.log(numbers);
 
-for (let i = 1; i < arr.length; i += 2) {
-    arr[i] *= 2;
+for (let i = 1; i < numbers.length; i += 2) {
+    numbers[i] *= 2;
 }
 
-console.log(arr);
+console.log(numbers);
+
+/**
+ * #2 Задача. Функція для пошуку мінімального значення в масиві
+ * Створіть функцію findMin, яка приймає масив чисел і повертає мінімальне значення з цього масиву.
+ */
+
+console.log(
+    "******************************\n" +
+        "Task 2\n" +
+        "******************************",
+);
+
+const numbersArr = [17, 88, 3, 15, 11, 33];
+
+console.log(numbersArr);
+
+function findMin(numbers) {
+    return Math.min.apply(null, numbers);
+}
+
+console.log("min number:", findMin(numbersArr));
+
+function findMin2(numbers) {
+    return Math.min(...numbers);
+}
+
+console.log("min number:", findMin2(numbersArr));
+
+function findMin3(numbers) {
+    let minNumber = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] < minNumber) {
+            minNumber = numbers[i];
+        }
+    }
+    return minNumber;
+}
+
+console.log("min number:", findMin3(numbersArr));
+
+function findMin4(numbers) {
+    let minNumber = numbers[0];
+    for (let number of numbers) {
+        if (number < minNumber) {
+            minNumber = number;
+        }
+    }
+    return minNumber;
+}
+
+console.log("min number:", findMin4(numbersArr));
+
+function findMin5(numbers) {
+    let minNumber = numbers[0];
+    numbers.forEach((number) => {
+        if (number < minNumber) {
+            minNumber = number;
+        }
+    });
+    return minNumber;
+}
+
+console.log("min number:", findMin5(numbersArr));
