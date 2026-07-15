6de99be5575ef5e86a783c226ce965894151d31a
604vadym <604vadym@proton.me>
2026-07-15 19:22:29 +0300

    feat: implement Task - Map

--- javascript-start-javascript-core/09-homework-map/script/main.js
+++ javascript-start-javascript-core/09-homework-map/script/main.js
@@ -24,3 +24,23 @@ console.log(
  *
  * 4000000, 2500000
  */
+
+const links = new Map([
+    ["https://www.youtube.com/watch?v=a1b2c3d4", 12_000],
+    ["https://www.youtube.com/watch?v=q1w2e3r5", 456_000],
+    ["https://www.youtube.com/watch?v=a1s2d3f5", 4_000_000],
+    ["https://www.youtube.com/watch?v=asd12eas", 2_500_000],
+    ["https://www.youtube.com/watch?v=vcbnd43g", 330_000],
+]);
+
+let viewsMillion = [];
+
+for (const views of links.values()) {
+    if (views >= 1_000_000) {
+        viewsMillion.push(views);
+    }
+}
+
+const output = viewsMillion.join(", ");
+
+console.log(output);
