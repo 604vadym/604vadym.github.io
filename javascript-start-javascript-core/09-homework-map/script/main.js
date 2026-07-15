189e8636a746f8bcc39996a83893b77b501e210d
604vadym <604vadym@proton.me>
2026-07-15 18:41:03 +0300

    chore: create new project - Map

---
+++ javascript-start-javascript-core/09-homework-map/script/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log(
+    "JavaScript Core #9. Домашнє завдання. Основи JavaScript: Структура даних Map",
+);
+
+/**
+ * Завдання
+ * Створити map де
+ *
+ * key - це посилання на відео на YouTube
+ * value - це кількість переглядів
+ * Вивести на екран тільки ті відео у яких більше 1 мільйона переглядів
+ * Вхідні дані:
+ *
+ *        const links = new Map([
+ *            ["https://www.youtube.com/watch?v=a1b2c3d4", 12_000],
+ *            ["https://www.youtube.com/watch?v=q1w2e3r5", 456_000],
+ *            ["https://www.youtube.com/watch?v=a1s2d3f5", 4_000_000],
+ *            ["https://www.youtube.com/watch?v=asd12eas", 2_500_000],
+ *           ["https://www.youtube.com/watch?v=vcbnd43g", 330_000],
+ *        ]);
+ * Output:
+ *
+ * 4000000, 2500000
+ */
