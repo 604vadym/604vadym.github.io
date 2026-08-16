01b49746fc99895c31e3463efda04f75371f37ad
604vadym <604vadym@proton.me>
2026-08-16 12:44:29 +0300

    feat: initialise constructor with DOM validation
    (fullstack-js): [smart adaptive slider-showcase-player via prototypes]
    - Create prototype-based "Slider" constructor accepting single and group
    selectors
    - Integrate external helper function "isDOMElementsFound()" for DOM
    verification
    - Implement immediate error throwing upon missing critical layout
    elements
    - Maintain initial architecture ready for future separation of concerns

--- fullstack-js/JavaScript/11-homework-fsm-engine-slider-showcase-player-oop-prototypes-classes/on-prototypes-oldschool-goa-trance/script/main.js
+++ fullstack-js/JavaScript/11-homework-fsm-engine-slider-showcase-player-oop-prototypes-classes/on-prototypes-oldschool-goa-trance/script/main.js
@@ -23,3 +23,125 @@
  * Переконайтеся, що ваш слайдер адаптований для роботи в різних браузерах та на різних пристроях.
  *
  */
+
+function isDOMElementsFound({ elements = null, collections = null } = {}) {
+    if (!elements && !collections) {
+        console.warn(`isDOMElementsFound(): invalid function call`);
+        return false;
+    }
+
+    if (elements) {
+        for (const [name, element] of Object.entries(elements)) {
+            if (!element) {
+                console.error(`DOM Error: element ${name} not found`);
+                return false;
+            }
+        }
+    }
+
+    if (collections) {
+        for (const [name, element] of Object.entries(collections)) {
+            if (element.length === 0) {
+                console.error(`DOM Error: elements ${name} not found`);
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function Slider(options) {
+    this._slider = document.querySelector(options.singleSelectors.slider);
+    this._track = document.querySelector(options.singleSelectors.track);
+    this._btnAutoscrollOn = document.querySelector(
+        options.singleSelectors.btnAutoscrollOn,
+    );
+    this._btnAutoscrollOff = document.querySelector(
+        options.singleSelectors.btnAutoscrollOff,
+    );
+    this._pagination = document.querySelector(
+        options.singleSelectors.pagination,
+    );
+    this._btnAudioNext = document.querySelector(
+        options.singleSelectors.btnAudioNext,
+    );
+    this._btnAudioPrev = document.querySelector(
+        options.singleSelectors.btnAudioPrev,
+    );
+    this._audioTrackTitle = document.querySelector(
+        options.singleSelectors.audioTrackTitle,
+    );
+    this._audioTrackCurrentTime = document.querySelector(
+        options.singleSelectors.audioTrackCurrentTime,
+    );
+    this._linkShop = document.querySelector(options.singleSelectors.linkShop);
+    this._slides = document.querySelectorAll(options.groupSelectors.slides);
+
+    if (
+        !isDOMElementsFound({
+            elements: {
+                slider: this._slider,
+                track: this._track,
+                btnAutoscrollOn: this._btnAutoscrollOn,
+                btnAutoscrollOff: this._btnAutoscrollOff,
+                pagination: this._pagination,
+                btnAudioNext: this._btnAudioNext,
+                btnAudioPrev: this._btnAudioPrev,
+                audioTrackTitle: this._audioTrackTitle,
+                audioTrackCurrentTime: this._audioTrackCurrentTime,
+                linkShop: this._linkShop,
+
+                viewport: document.querySelector(
+                    options.singleSelectors.viewport,
+                ),
+                btnNext: document.querySelector(
+                    options.singleSelectors.btnNext,
+                ),
+                btnPrev: document.querySelector(
+                    options.singleSelectors.btnPrev,
+                ),
+                btnAudioPlay: document.querySelector(
+                    options.singleSelectors.btnAudioPlay,
+                ),
+                btnAudioPause: document.querySelector(
+                    options.singleSelectors.btnAudioPause,
+                ),
+                audioTrackFullTime: document.querySelector(
+                    options.singleSelectors.audioTrackFullTime,
+                ),
+            },
+            collections: {
+                slides: this._slides,
+
+                images: document.querySelectorAll(
+                    options.groupSelectors.images,
+                ),
+            },
+        })
+    )
+        throw new Error("DOM elements validation failed");
+}
+
+const slider = new Slider({
+    singleSelectors: {
+        slider: ".slider",
+        track: ".slider__track",
+        btnAutoscrollOn: ".slider__btn--autoscroll-on",
+        btnAutoscrollOff: ".slider__btn--autoscroll-off",
+        pagination: ".slider__pagination",
+        btnAudioNext: ".slider__btn-audio--next",
+        btnAudioPrev: ".slider__btn-audio--prev",
+        audioTrackTitle: ".slider__audio-track-title",
+        audioTrackCurrentTime: ".slider__audio-track-current-time",
+        linkShop: ".slider__link-shop",
+        viewport: ".slider__viewport",
+        btnNext: ".slider__btn--next",
+        btnPrev: ".slider__btn--prev",
+        btnAudioPlay: ".slider__btn-audio--play",
+        btnAudioPause: ".slider__btn-audio--pause",
+        audioTrackFullTime: ".slider__audio-track-full-time",
+    },
+
+    groupSelectors: { slides: ".slider__slide", images: ".slider__image" },
+});
