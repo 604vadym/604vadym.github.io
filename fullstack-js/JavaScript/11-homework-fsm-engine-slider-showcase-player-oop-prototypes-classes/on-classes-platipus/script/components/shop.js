76a1fbd53b9593d95ee628d8548b4d69078c3b52
604vadym <604vadym@proton.me>
2026-09-07 17:59:52 +0300

    refactor: migrate "Shop" to ES6 classes
    (fullstack-js): [smart adaptive slider-showcase-player via classes]

--- fullstack-js/JavaScript/11-homework-fsm-engine-slider-showcase-player-oop-prototypes-classes/on-classes-platipus/script/components/shop.js
+++ fullstack-js/JavaScript/11-homework-fsm-engine-slider-showcase-player-oop-prototypes-classes/on-classes-platipus/script/components/shop.js
@@ -4,37 +4,21 @@ import * as helper from "../utils/helpers.js";
 import DOMValidator from "../services/dom-validator.js";
 import KeyboardManager from "../services/keyboard-manager.js";
 
-export default function Shop(options) {
-    this._options = options;
+const DEFAULT_URL = "https://bandcamp.com";
 
-    Object.defineProperty(this, "_domValidator", {
-        value: new DOMValidator(Shop),
-        writable: false,
-        configurable: false,
-    });
-
-    Object.defineProperty(this, "_keyboardManager", {
-        value: new KeyboardManager(),
-        writable: false,
-        configurable: false,
-    });
-}
-
-Object.defineProperty(Shop, "DEFAULT_URL", {
-    value: "https://bandcamp.com",
-    writable: false,
-    configurable: false,
-});
-
-Shop.prototype = {
-    constructor: Shop,
+export default class Shop {
+    constructor(options) {
+        this._options = options;
+        this._domValidator = new DOMValidator(Shop);
+        this._keyboardManager = new KeyboardManager();
+    }
 
     init() {
         this._initDOMElements();
         this._initProps();
         this._updateUrl();
         this._keyboardManager.init(this, "press");
-    },
+    }
 
     setActiveIndex(index) {
         const isValidIndex =
@@ -51,11 +35,11 @@ Shop.prototype = {
             this._currentIndex = null;
             this._link.setAttribute("href", this._defaultUrl);
         }
-    },
+    }
 
     handleKeyDown(e) {
         return this._keyboardManager.manage(e);
-    },
+    }
 
     _initDOMElements(childElements) {
         const link = document.querySelector(this._options.singleSelectors.link);
@@ -65,58 +49,57 @@ Shop.prototype = {
         });
 
         this._link = link;
-    },
+    }
 
     _initProps() {
         this._currentIndex = 0;
         this._initDefaultUrl();
         this._initData();
-    },
+    }
 
     _updateUrl() {
         const currentUrl = this._getUrl();
         if (this._link.getAttribute("href") !== currentUrl) {
             this._link.setAttribute("href", currentUrl);
         }
-    },
+    }
 
     _getUrl() {
         return this._currentIndex !== null
             ? this._data[this._currentIndex].url
             : this._defaultUrl;
-    },
+    }
 
     _pressExecute(e) {
         helper.prevent(e);
         window.open(this._getUrl(), "_blank");
         return true;
-    },
+    }
 
     _initDefaultUrl() {
         const configUrl = this._options.defaultUrl;
         const markupUrl = this._link.getAttribute("href");
-        this._defaultUrl =
-            configUrl || markupUrl || this.constructor.DEFAULT_URL;
+        this._defaultUrl = configUrl || markupUrl || DEFAULT_URL;
         this._validateDefaultUrl(configUrl, markupUrl);
-    },
+    }
 
     _validateDefaultUrl(configUrl, markupUrl) {
         if (!helper.isValidUrl(this._defaultUrl)) {
             throw new Error(
                 `[Shop]: Master initialisation aborted\n` +
                     `Both custom options.defaultUrl("${configUrl})", markup ("${markupUrl}") ` +
-                    `and fallback DEFAULT_URL ("${this.constructor.DEFAULT_URL}")\n` +
+                    `and fallback DEFAULT_URL ("${DEFAULT_URL}")\n` +
                     `Failed to validate against the required URL specification protocol\n` +
                     `Expected format: "http://example.com" or "https://example.com" ` +
                     `(must include valid transfer protocol and domain name)`,
             );
         }
-    },
+    }
 
     _initData() {
         this._data = this._options.data;
         this._validateData();
-    },
+    }
 
     _validateData() {
         if (
@@ -153,5 +136,5 @@ Shop.prototype = {
                 );
             }
         });
-    },
-};
+    }
+}
