d7e2e5fe7e3c78e89e4e1a600a25775dff435972
604vadym <604vadym@proton.me>
2026-09-17 13:00:30 +0300

    feat: implement Task 40 - map JSON to object
    (stashchuk-course)

--- javascript-bogdan-stashchuk-course/40-map-json-to-object/finish.js
+++ javascript-bogdan-stashchuk-course/40-map-json-to-object/finish.js
@@ -8,10 +8,33 @@
  *
  * 4. Выведите в консоль "commentsQuantity" последнего объекта
  */
+"use strict";
 
 const postsJSON = [
-  '{"postId":1355,"commentsQuantity":5}',
-  '{"postId":5131,"commentsQuantity":13}',
-  '{"postId":6134,"commentsQuantity":2}',
-  '{"postId":2351,"commentsQuantity":8}',
-]
+    '{"postId":1355,"commentsQuantity":5}',
+    '{"postId":5131,"commentsQuantity":13}',
+    '{"postId":6134,"commentsQuantity":2}',
+    '{"postId":2351,"commentsQuantity":8}',
+];
+
+const posts = new Array(postsJSON.length);
+
+postsJSON.forEach((json, i) => (posts[i] = JSON.parse(json)));
+
+console.log(posts);
+console.log(posts[1].postId);
+console.log(posts[posts.length - 1].commentsQuantity);
+
+console.log("******************************");
+
+const postsViaMap = postsJSON.map((post) => JSON.parse(post));
+console.log(postsViaMap);
+console.log(postsViaMap[1].postId);
+console.log(postsViaMap[postsViaMap.length - 1].commentsQuantity);
+
+console.log("******************************");
+
+const postsViaMapShort = postsJSON.map(JSON.parse);
+console.log(postsViaMapShort);
+console.log(postsViaMapShort[1].postId);
+console.log(postsViaMapShort[postsViaMapShort.length - 1].commentsQuantity);
