395fbff2bbb297e0f59be428f806ee6e48f78fdc
604vadym <604vadym@proton.me>
2026-09-18 11:26:48 +0300

    feat: implement Task 48 - reduce to object
    (stashchuk-course)

--- javascript-bogdan-stashchuk-course/48-reduce-to-object/finish.js
+++ javascript-bogdan-stashchuk-course/48-reduce-to-object/finish.js
@@ -8,33 +8,44 @@
  */
 
 const inputProducts = [
-  {
-    title: 'Phone case',
-    price: 23,
-    quantity: 2,
-    category: 'Accessories',
-  },
-  {
-    title: 'Android phone',
-    price: 150,
-    quantity: 1,
-    category: 'Phones',
-  },
-  {
-    title: 'Headphones',
-    price: 78,
-    quantity: 1,
-    category: 'Accessories',
-  },
-  {
-    title: 'Sport Watch',
-    price: 55,
-    quantity: 2,
-    category: 'Watches',
-  },
-]
+    {
+        title: "Phone case",
+        price: 23,
+        quantity: 2,
+        category: "Accessories",
+    },
+    {
+        title: "Android phone",
+        price: 150,
+        quantity: 1,
+        category: "Phones",
+    },
+    {
+        title: "Headphones",
+        price: 78,
+        quantity: 1,
+        category: "Accessories",
+    },
+    {
+        title: "Sport Watch",
+        price: 55,
+        quantity: 2,
+        category: "Watches",
+    },
+];
 
-console.log(quantitiesByCategories(inputProducts))
+function quantitiesByCategories(products) {
+    return products.reduce((categories, product) => {
+        if (product.category in categories) {
+            categories[product.category] += product.quantity;
+        } else {
+            categories[product.category] = product.quantity;
+        }
+        return categories;
+    }, {});
+}
+
+console.log(quantitiesByCategories(inputProducts));
 /* {
   Accessories: 3,
   Phones: 1,
