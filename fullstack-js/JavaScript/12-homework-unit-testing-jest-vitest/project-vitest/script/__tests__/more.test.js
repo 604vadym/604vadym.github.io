dea6807373a23fd843e050325b2e3a66df33c6ee
604vadym <604vadym@proton.me>
2026-09-20 16:14:12 +0300

    test: refactor "confirmAndSend()" suite
    (fullstack-js): [Unit Testing. Jest. Vitest]
    - Fix sad path test by simulating clicks on wrong ID targets to ensure
    no listeners are leaks
    - Move shared button generation into the "beforeEach()" hook to reduce
    code duplication

--- fullstack-js/JavaScript/12-homework-unit-testing-jest-vitest/project-vitest/script/__tests__/more.test.js
+++ fullstack-js/JavaScript/12-homework-unit-testing-jest-vitest/project-vitest/script/__tests__/more.test.js
@@ -28,9 +28,13 @@ describe("Module: more", () => {
     });
 
     describe("Function: confirmAndSend()", () => {
-        const buttonId = "testButton";
+        const correctBtnId = "correct-id";
+        let button = null;
 
         beforeEach(() => {
+            button = document.createElement("button");
+            button.id = correctBtnId;
+            document.body.appendChild(button);
             vi.useFakeTimers();
         });
 
@@ -41,10 +45,6 @@ describe("Module: more", () => {
         });
 
         test("trigger onConfirm callback with accurate structured event data and timestamp on click", () => {
-            const button = document.createElement("button");
-            button.id = buttonId;
-            document.body.appendChild(button);
-
             const mockCallback = vi.fn();
             const mockDate = new Date(2026, 2, 23);
             vi.setSystemTime(mockDate);
@@ -54,7 +54,7 @@ describe("Module: more", () => {
                 timestamp: mockDate.valueOf(),
             };
 
-            const result = more.confirmAndSend(buttonId, mockCallback);
+            const result = more.confirmAndSend(correctBtnId, mockCallback);
             expect(result).toBe(true);
 
             button.click();
@@ -62,11 +62,14 @@ describe("Module: more", () => {
             expect(mockCallback).toHaveBeenCalledWith(expectedData);
         });
 
-        test("return false and prevent registration when the targets button is absent in DOM", () => {
+        test("return false and do not attach listener if button ID does not match", () => {
+            const wrongBtnId = "wrong-id";
             const mockCallback = vi.fn();
 
-            const result = more.confirmAndSend(buttonId, mockCallback);
+            const result = more.confirmAndSend(wrongBtnId, mockCallback);
             expect(result).toBe(false);
+
+            button.click();
             expect(mockCallback).not.toHaveBeenCalled();
         });
     });
