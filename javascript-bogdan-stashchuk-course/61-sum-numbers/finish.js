4a26979cb259607fe6eb1ccbf35a30abbfd9d1e5
604vadym <604vadym@proton.me>
2026-09-21 22:08:17 +0300

    feat: implement Task 61 - sum numbers
    (stashchuk-course)

--- javascript-bogdan-stashchuk-course/61-sum-numbers/finish.js
+++ javascript-bogdan-stashchuk-course/61-sum-numbers/finish.js
@@ -7,12 +7,22 @@
  *
  * 3. Также верните эту сумму как результат функции
  */
+"use strict";
 
-sumNumbers(1, 3)
+function sumNumbers() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    console.log(sum);
+    return sum;
+}
+
+sumNumbers(1, 3);
 // 4
 
-sumNumbers(10, 20, 5)
+sumNumbers(10, 20, 5);
 // 35
 
-sumNumbers(2, 5, 80, 1, 10, 12)
+sumNumbers(2, 5, 80, 1, 10, 12);
 // 110
