494c3cf810fb41af1e3051bc8d5ed151812f5e01
604vadym <604vadym@proton.me>
2026-09-24 11:24:50 +0300

    feat(js-core): implement additional task

--- javascript-start-javascript-core/17.homework-date/script/main.js
+++ javascript-start-javascript-core/17.homework-date/script/main.js
@@ -9,8 +9,43 @@ console.log("JavaScript Core #17. Домашнє завдання. Робота 
  * Важливо: в цьому завданні допускається розбіг з реальним значенням в +/- 1 рік, через округлення, у даному випадку з 113.5 до 114.
  */
 
+console.log(
+    "******************************\n" +
+        "Task 1\n" +
+        "******************************",
+);
+
 const dateOfBirth = new Date("1991-02-23");
 const ageMs = Date.now() - dateOfBirth.getTime();
 const age = Math.floor(ageMs / 1000 / 60 / 60 / 24 / 365.2425);
 
 console.log(age + " years");
+
+/**
+ * Дано додатне число, повернути суму його цифр.
+ *
+ * Приклади:
+ *
+ * 1234 → 10
+ * 100 → 1
+ * 777 → 21
+ */
+
+console.log(
+    "******************************\n" +
+        "Task 2\n" +
+        "******************************",
+);
+
+function sumOfDigits(number) {
+    let sum = 0;
+    while (number) {
+        sum += number % 10;
+        number = Math.floor(number / 10);
+    }
+    return sum;
+}
+
+console.log(sumOfDigits(1234)); // → 10
+console.log(sumOfDigits(100)); // → 1
+console.log(sumOfDigits(777)); // → 21
