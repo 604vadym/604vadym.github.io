454e85008fa0ffb6763338681d822c9593fed31b
604vadym <604vadym@proton.me>
2026-09-26 18:11:32 +0300

    refactor: reimplement "putData()" logic the way I consider correct
     (fullstack-js): [async/await. Fetch API]

--- fullstack-js/JavaScript/14.homework-async-await-fetch-api/script/main.js
+++ fullstack-js/JavaScript/14.homework-async-await-fetch-api/script/main.js
@@ -4,6 +4,7 @@ console.log("JS #14. Асинхронні операції та робота з 
 
 const httpErrorMsg = "HTTP error! status:";
 const url = "https://jsonplaceholder.typicode.com";
+const urlPosts = "https://jsonplaceholder.typicode.com/posts/";
 
 async function getData(segment) {
     const response = await fetch(`${url}${segment}`);
@@ -76,58 +77,45 @@ try {
     console.error(error.message);
 }
 
-/*
- *
- * #3
- *
- * Функціональні вимоги:
- *
- * 1. Вхідні параметри:
- *  - `id`: Ідентифікатор об'єкта, який потрібно оновити.
- *  - `data`: Об'єкт з даними для оновлення.
- *
- * 2. Виконання запиту:
- *  - Виконати асинхронний HTTP PUT запит до `https://jsonplaceholder.typicode.com/posts/${id}` з використанням `id` та `data`.
- *  - Встановити заголовок `Content-Type: application/json`.
- * 3. Обробка відповідей:
- *  - У разі успішної відповіді, конвертувати відповідь у формат JSON і повернути отримані дані.
- *  - Якщо відповідь вказує на помилку (наприклад, неіснуючий ресурс або проблеми з сервером), повернути повідомлення про помилку.
- *
- * 4. Логування:
- *  - Логувати у консоль результат або повідомлення про помилку.
- *
- * Технічні Вимоги:
- * - Використання асинхронних функцій (`async/await`) для обробки HTTP запитів.
- * - Належне управління помилками та відповідями від API.
- *
- */
-
 async function putData(id, data) {
-    try {
-        const response = await fetch(
-            `https://jsonplaceholder.typicode.com/posts/${id}`,
-            {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            },
-        );
+    const response = await fetch(`${urlPosts}${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
 
-        if (!response.ok) {
-            console.error("HTTP error! status:", response.status);
-            return `HTTP error! status: ${response.status}`;
-        }
-
-        const responseData = await response.json();
-        console.log("PUT data:", responseData);
-
-        return responseData;
-    } catch (error) {
-        console.error(error);
-        return error.message;
+    if (!response.ok) {
+        throw new Error(`${httpErrorMsg} ${response.status}`);
     }
+
+    return response.json();
+}
+
+putData(33, { userName: "Fred", role: "user" })
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error.message));
+
+putData(-33, { userName: "derF", role: "user" })
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error.message));
+
+try {
+    console.log(await putData(88, { userName: "Mike", role: "user" }));
+} catch (error) {
+    console.error(error.message);
+}
+
+try {
+    console.log(
+        await putData(-88, {
+            userName: "ekiM",
+            role: "user",
+        }),
+    );
+} catch (error) {
+    console.error(error.message);
 }
 
 /*
